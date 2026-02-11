80f3e8ba5f80998d5a1030f546a98bda2f7889a0
Tristan Duret Robert <tristan.duret-robert@r05p05.v302.vj.sm.cri.epita.fr>
2026-02-11 15:48:21 +0100

    final 2

--- integration.c
+++ integration.c
@@ -8,7 +8,10 @@
 
 bool setQuadFormula(QuadFormula* qf, char* name)
 {
-  return true;
+	if (strcmp("right", name)&&strcmp("left", name)&&strcmp("middle", name)&&strcmp("gauss2", name)&&strcmp("gauss3", name)&&strcmp("simpson", name)&&strcmp("trapezes", name)) return false;
+
+	qf->name=name;
+	return true;
 }
 
 /* This function is not required ,but it may useful to debug */
@@ -27,8 +30,42 @@ void printQuadFormula(QuadFormula* qf)
 */
 double integrate(double (*f)(double), double a, double b, int N, QuadFormula* qf)
 {
-  return 0.0;
+	double result;
+	if (strcmp("left", name)) result = left(f,a,b,N);
+	return 0.0;
 }
+
+double left (double (*f)(double), double a, double b, int N){
+	double result=0, a_aux,b_aux;
+	for (int i=0; i<N; i++){
+
+		a_aux = a +i *((b-a)/N);
+		b_aux = a+(i+1)*((b-a)/N);
+
+		result += (b_aux-a_aux)*f(a_aux);
+	}
+	return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 double integrate_dx(double (*f)(double), double a, double b, double dx, QuadFormula* qf)
 {
--- test_integration.c
+++ test_integration.c
@@ -6,8 +6,20 @@
 #include "integration.h"
 
 /* Code here everything you need to test the integration methods and show your numericzal results */
-
+#include <math.h>
+double f(double t)
+{
+return sin(t*t);
+}
 int main()
 {
-  return 0;
+ QuadFormula qf;
+double I1, I2, I3;
+setQuadFormula(&qf, "trapezes");
+I1 = integrate(f, -1, 4, 10, &qf);
+setQuadFormula(&qf, "middle");
+I2 = integrate(f, -1, 4, 10, &qf);
+setQuadFormula(&qf, "simpson");
+I3 = integrate_dx(f, -1, 4, 0.1, &qf);
+	return 0;
 }
