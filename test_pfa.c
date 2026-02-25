cbcb7032e059489064b6b9ea8c4c17b5057da60b
Tristan Duret Robert <tristan.duret-robert@r05p05.v302.vj.sm.cri.epita.fr>
2026-02-25 15:41:59 +0100

    ERTG

--- test_pfa.c
+++ test_pfa.c
@@ -8,5 +8,7 @@
 /* Code here anything you want to test your pfa functions and to show your numerical results */
 int main()
 {
+	init_integration("trapezes", 0.1);
+	printf("%.3f",PHI(1.96));	//0.975
   return 0;
 }
